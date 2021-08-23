c034bef3f83f393c2358bb7c59a16b3434cf0083
rihu-5 <86797300+rihu-5@users.noreply.github.com>
2021-08-23 23:29:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,15 +100,14 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("Other Bots", url="https://t.me/dobby_free")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
                     ]
                 ]
             )
-        )
-
+        ) 
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
@@ -196,4 +195,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
         ]
         ]
-    await message.reply(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>Developer : <a href='https://t.me/Rihu_mone'>RIHU</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://t.me/BWF_ofc'>Click here</a>\nUpdate Channel : <a href='https://t.me/Bwf_Movies'>BwF_Movies</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
